c29c57ed22e79548c6927aa841d9efd0846efe02
kote6725@gmail.com <kote6725@gmail.com>
2019-03-10 21:28:28 +0200

    Добавьте файлы проекта.

---
+++ Csharp_lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = SquareSum(4, 3);
+            Console.WriteLine("Ответ: {0}", res);
+            Console.ReadKey();
+        }
+
+        static double SquareSum(double a, double b)
+        {
+            double sum = a + b;
+            double result = Math.Pow(sum, 2);
+            return result;
+        }
+    }
+}
